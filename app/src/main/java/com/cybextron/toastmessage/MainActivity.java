51b384db94604f033f57fa6ef8c6cb50f524e1e1
SuryaKarmakar <surjokarmakarssj7@gmail.com>
2021-08-15 01:09:23 +0530

    update code

--- app/src/main/java/com/cybextron/toastmessage/MainActivity.java
+++ app/src/main/java/com/cybextron/toastmessage/MainActivity.java
@@ -3,7 +3,10 @@ package com.cybextron.toastmessage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -14,35 +17,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button bt1 = (Button) findViewById(R.id.btn1);
-        Button bt2 = (Button) findViewById(R.id.btn2);
-        Button bt3 = (Button) findViewById(R.id.btn3);
+        Button btn1 = (Button) findViewById(R.id.btn1);
+        Button btn2 = (Button) findViewById(R.id.btn2);
+        Button btn3 = (Button) findViewById(R.id.btn3);
+        Button btn4 = (Button) findViewById(R.id.btn4);
+        Button btn5 = (Button) findViewById(R.id.btn5);
 
-        Toast toast = Toast.makeText(getApplicationContext(), "This is Toast Object", Toast.LENGTH_SHORT);
+        //Creating the LayoutInflater instance
+        LayoutInflater li = getLayoutInflater();
+        //Getting the View object as defined in the custom_toast.xml file
+        View layout = li.inflate(R.layout.custom_toast,(ViewGroup) findViewById(R.id.customtoast));
 
-        // Make a toast object and show it with help of the show() function
-        bt1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                Toast toast = Toast.makeText(getApplicationContext(), "This is Toast Object", Toast.LENGTH_SHORT);
-                toast.show();
-            }
+        // Simple toast message
+        btn1.setOnClickListener(view -> {
+            Toast toast = Toast.makeText(getApplicationContext(), "Simple Toast Message", Toast.LENGTH_SHORT);
+            toast.show();
         });
 
-        // Call the toast in one line
-        bt2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "This is One Line Toast", Toast.LENGTH_SHORT).show();
-            }
+        // Show long duration toast message with methode chaining
+        btn2.setOnClickListener(view -> {
+            Toast.makeText(getApplicationContext(), "Show Long Duration Toast", Toast.LENGTH_LONG).show();
         });
 
-        // Pass the toast message into function
-        bt3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showToast("Pass Toast Message");
-            }
+        btn3.setOnClickListener(view -> {
+            Toast toast = Toast.makeText(getApplicationContext(), "Set Toast Position", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.BOTTOM | Gravity.LEFT, 10, 20);
+            toast.show();
+        });
+
+        btn4.setOnClickListener(view -> {
+            Toast toast = Toast.makeText(getApplicationContext(), "Set Toast Position", Toast.LENGTH_SHORT);
+            toast.setView(layout);
+            toast.show();
+        });
+
+        btn5.setOnClickListener(view -> {
+
         });
     }
     public void showToast (String msg){
